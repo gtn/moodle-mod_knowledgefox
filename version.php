23e41b174b05f290b4350c6b37bd31d3d24055bd
dprieler <dprieler@gmail.com>
2016-10-19 17:14:47 +0200

    first version

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_knowledgefox';
+$plugin->version = 2016100100;
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release  = "1.0.0";
+$plugin->requires = 2011033005;
+$plugin->cron      = 0;
