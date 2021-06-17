5dc514bbfa005b25e934ae9b39a61395f811ee0e
Dietmar Angerer <uab.katzinger@gmail.com>
2021-06-17 10:45:12 +0200

    Weiter Link, neues Fenster

--- view.php
+++ view.php
@@ -152,7 +152,7 @@ if (knowledgefox_is_student($course->id,$USER->id)){
 		$mess.= '<p>Dieser Lerninhalt befindet sich auf einem verbundenen Knowledgefox Server.<p>';
 		$mess.= '<p>Bitte klicken sie unten auf "weiter" und sie werden zu Knowledgefox weitergeleitet.<br>'; 
 		$mess.= 'Auf der aufgerufenen Knowledgefox Anmeldeseite klicken sie bitte auf "Anmelden mit Moodle".</p>';
-		$mess.= '<br><p style="font-size:x-large"> <a href="'.$wsparams->knowledgefoxserver.'">Weiter</a> zu Knowledgefox.</p>';
+		$mess.= '<br><p style="font-size:x-large"> <a target="_blank" href="'.$wsparams->knowledgefoxserver.'">Weiter</a> zu Knowledgefox.</p>';
 	}
 	/*$kf_completedcourses=knowledgefox_ws_get_user_grading($knowledgefox->lernpaket,$wsparams);
 	if (is_array($kf_completedcourses)) {
